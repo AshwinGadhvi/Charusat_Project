effefcbe67aa3ffb4ca0f89a1cf8427225c95292
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-08-29 20:09:17 +0530

    Writing Some Code For Show Casing Session Value.

--- Admin/Admin.master.vb
+++ Admin/Admin.master.vb
@@ -1,5 +1,15 @@
 ﻿
 Partial Class Admin
     Inherits System.Web.UI.MasterPage
+
+    Private Sub Admin_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            If Session("admin_name") IsNot Nothing Then
+                lblAdminName.Text = "Welcome, " & Session("admin_name")
+            Else
+                lblAdminName.Text = "Welcome, Guest!"
+            End If
+        End If
+    End Sub
 End Class
 
