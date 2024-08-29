c0b21901f5974f26fb5e93a8130506901ec16de6
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-08-29 20:09:59 +0530

    Writing Some Session Management Code.

---
+++ Admin/User.master.vb
@@ -0,0 +1,15 @@
+﻿
+Partial Class Admin_User
+    Inherits System.Web.UI.MasterPage
+
+    Private Sub Admin_User_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            If Session("user_name") IsNot Nothing Then
+                lbluser.Text = "Welcome, " & Session("user_name")
+            Else
+                lbluser.Text = "Welcome, Guest!"
+            End If
+        End If
+    End Sub
+End Class
+
