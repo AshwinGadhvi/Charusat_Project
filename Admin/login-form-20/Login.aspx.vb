a3aefdeb70dcb379fb22f697aa78dbc12fe7f4da
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-08-29 20:08:12 +0530

    Adding Some Stuff Related Auth.

--- Admin/login-form-20/Login.aspx.vb
+++ Admin/login-form-20/Login.aspx.vb
@@ -8,14 +8,23 @@ Partial Class login_form_20_Login
     Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
         Dim d As New Dao
         Dim ds As New Data.DataSet
+        Dim ds1 As New Data.DataSet
+        Dim ds2 As New Data.DataSet
         ds = d.getData("select * from Company_Master where company_email='" & company_email.Text & "' and company_password='" & company_password.Text & "' ")
-
+        ds1 = d.getData("select * from Admin_Master where user_name ='" & company_email.Text & "' and user_password ='" & company_password.Text & "' and user_type='Admin'")
+        ds2 = d.getData("select * from Admin_Master where user_name ='" & company_email.Text & "' and user_password ='" & company_password.Text & "' and user_type='User'")
 
 
         If ds.Tables(0).Rows.Count > 0 Then
             Session("u_id") = ds.Tables(0).Rows(0)("company_id").ToString()
             Session("u_name") = ds.Tables(0).Rows(0)("company_name").ToString()
             Response.Redirect("../Company_Dashboard.aspx")
+        ElseIf ds1.Tables(0).Rows.Count > 0 Then
+            Session("admin_name") = ds1.Tables(0).Rows(0)("user_name").ToString()
+            Response.Redirect("../Home.aspx")
+        ElseIf ds2.Tables(0).Rows.Count > 0 Then
+            Session("user_name") = ds1.Tables(0).Rows(0)("user_name").ToString()
+            'Response.Redirect("../Home.aspx")
         Else
             company_email.Text = ""
             lbl_msg.Text = "Incorrect Id And Password..!!!!" 'popup
