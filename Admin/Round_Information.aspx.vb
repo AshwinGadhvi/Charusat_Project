2498e812902365994a38a54319ae7230a56ece2e
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-08-30 22:13:31 +0530

    Backend Code For Getting The Information Or Session Data For Adding Some Credential Based Task.

--- Admin/Round_Information.aspx.vb
+++ Admin/Round_Information.aspx.vb
@@ -2,4 +2,12 @@
 Partial Class Admin_Round_Information
     Inherits System.Web.UI.Page
 
+    Private Sub Admin_Round_Information_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim d As New Dao
+        Dim ds As New Data.DataSet
+        ds = d.getData("select company_id from Company_Details where c_id='" & Session("u_id") & "'")
+        If ds.Tables(0).Rows.Count > 0 Then
+            company_id.Text = ds.Tables(0).Rows(0)("company_id")
+        End If
+    End Sub
 End Class
