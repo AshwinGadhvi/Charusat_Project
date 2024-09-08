e9173cf20a072ca713b2a5369e51bd3fcbb1f623
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-09-09 01:30:59 +0530

    adding current time.

--- Main.aspx.vb
+++ Main.aspx.vb
@@ -2,4 +2,12 @@
 Partial Class Main
     Inherits System.Web.UI.Page
 
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Set the current date to filter jobs that haven't expired
+        SqlDataSource1.SelectParameters("CurrentDate").DefaultValue = DateTime.Now.ToString("yyyy-MM-dd")
+
+    End Sub
+
+
+
 End Class
