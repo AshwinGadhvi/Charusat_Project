7826507ce69a6e4b9dada6fe6d4a683558e92145
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-09-09 10:15:24 +0530

    writing back end code for show casing time for last updated.

--- Main.aspx.vb
+++ Main.aspx.vb
@@ -1,13 +1,30 @@
-﻿
-Partial Class Main
+﻿Partial Class Main
     Inherits System.Web.UI.Page
 
+    ' Method to calculate time difference
+    Protected Function CalculatePostedTime(ByVal postDate As Object) As String
+        If postDate IsNot Nothing Then
+            Dim postedDate As DateTime = Convert.ToDateTime(postDate)
+            Dim timeDifference As TimeSpan = DateTime.Now - postedDate
+
+            ' Calculate how long ago the job was posted
+            If timeDifference.TotalDays >= 1 Then
+                Return String.Format("{0} day(s) ago", Math.Floor(timeDifference.TotalDays))
+            ElseIf timeDifference.TotalHours >= 1 Then
+                Return String.Format("{0} hour(s) ago", Math.Floor(timeDifference.TotalHours))
+            ElseIf timeDifference.TotalMinutes >= 1 Then
+                Return String.Format("{0} minute(s) ago", Math.Floor(timeDifference.TotalMinutes))
+            Else
+                Return "Just now"
+            End If
+        Else
+            Return String.Empty
+        End If
+    End Function
+
+    ' Page Load event to set current date parameter
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles Me.Load
-        ' Set the current date to filter jobs that haven't expired
         SqlDataSource1.SelectParameters("CurrentDate").DefaultValue = DateTime.Now.ToString("yyyy-MM-dd")
-
     End Sub
-
-
 
 End Class
