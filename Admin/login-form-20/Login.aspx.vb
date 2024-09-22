ce3c05d43dd51e6e7e3a1e5bf28f79ff40df4c71
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-09-22 12:12:28 +0530

    Using Varify Method To Check Weather Password Enteres Was Correct Or Not.

--- Admin/login-form-20/Login.aspx.vb
+++ Admin/login-form-20/Login.aspx.vb
@@ -2,6 +2,8 @@
 Imports System.Net.Mail
 Imports System.Text
 Imports System.Security.Cryptography
+Imports BCrypt.Net ' Make sure to include the BCrypt library
+
 Partial Class login_form_20_Login
     Inherits System.Web.UI.Page
 
@@ -10,25 +12,60 @@ Partial Class login_form_20_Login
         Dim ds As New Data.DataSet
         Dim ds1 As New Data.DataSet
         Dim ds2 As New Data.DataSet
-        Dim ds3 As New Data.DataSet
-        ds = d.getData("select * from Company_Master where company_email='" & company_email.Text & "' and company_password='" & company_password.Text & "' ")
-        ds1 = d.getData("select * from Admin_Master where user_name ='" & company_email.Text & "' and user_password ='" & company_password.Text & "' and user_type='Admin'")
-        ds2 = d.getData("select * from Admin_Master where user_name ='" & company_email.Text & "' and user_password ='" & company_password.Text & "' and user_type='User'")
+
+        ' Check if the email exists in Company_Master
+        ds = d.getData("SELECT * FROM Company_Master WHERE company_email = '" & company_email.Text & "'")
         If ds.Tables(0).Rows.Count > 0 Then
-            Session("u_id") = ds.Tables(0).Rows(0)("company_id").ToString()
-            Session("u_name") = ds.Tables(0).Rows(0)("company_name").ToString()
-            Response.Redirect("../Company_Dashboard.aspx")
-        ElseIf ds1.Tables(0).Rows.Count > 0 Then
-            Session("admin_name") = ds1.Tables(0).Rows(0)("user_name").ToString()
-            Response.Redirect("../Home.aspx")
-        ElseIf ds2.Tables(0).Rows.Count > 0 Then
-            Session("user_name") = ds2.Tables(0).Rows(0)("user_name").ToString()
-            Response.Redirect("../employee.aspx")
-        Else
-            company_email.Text = ""
-            lbl_msg.Text = "Incorrect Id And Password..!!!!" 'popup
-            lbl_msg.ForeColor = Drawing.Color.White
-            company_email.Focus()
+            ' Verify password for Company_Master
+            If BCrypt.Net.BCrypt.Verify(company_password.Text, ds.Tables(0).Rows(0)("company_password").ToString()) Then
+                Session("u_id") = ds.Tables(0).Rows(0)("company_id").ToString()
+                Session("u_name") = ds.Tables(0).Rows(0)("company_name").ToString()
+                Response.Redirect("../Company_Dashboard.aspx")
+            Else
+                company_email.Text = ""
+                lbl_msg.Text = "Incorrect Id And Password..!!!!"
+                lbl_msg.ForeColor = Drawing.Color.White
+                company_email.Focus()
+                Return
+            End If
         End If
+
+        ' Check for Admin
+        ds1 = d.getData("SELECT * FROM Admin_Master WHERE user_name = '" & company_email.Text & "'")
+        If ds1.Tables(0).Rows.Count > 0 Then
+            ' Verify password for Admin_Master
+            If BCrypt.Net.BCrypt.Verify(company_password.Text, ds1.Tables(0).Rows(0)("user_password").ToString()) Then
+                Session("admin_name") = ds1.Tables(0).Rows(0)("user_name").ToString()
+                Response.Redirect("../Home.aspx")
+            Else
+                company_email.Text = ""
+                lbl_msg.Text = "Incorrect Id And Password..!!!!"
+                lbl_msg.ForeColor = Drawing.Color.White
+                company_email.Focus()
+                Return
+            End If
+        End If
+
+        ' Check for User
+        ds2 = d.getData("SELECT * FROM Admin_Master WHERE user_name = '" & company_email.Text & "' AND user_type = 'User'")
+        If ds2.Tables(0).Rows.Count > 0 Then
+            ' Verify password for User
+            If BCrypt.Net.BCrypt.Verify(company_password.Text, ds2.Tables(0).Rows(0)("user_password").ToString()) Then
+                Session("user_name") = ds2.Tables(0).Rows(0)("user_name").ToString()
+                Response.Redirect("../employee.aspx")
+            Else
+                company_email.Text = ""
+                lbl_msg.Text = "Incorrect Id And Password..!!!!"
+                lbl_msg.ForeColor = Drawing.Color.White
+                company_email.Focus()
+                Return
+            End If
+        End If
+
+        ' If no match found
+        company_email.Text = ""
+        lbl_msg.Text = "Incorrect Id And Password..!!!!"
+        lbl_msg.ForeColor = Drawing.Color.White
+        company_email.Focus()
     End Sub
 End Class
