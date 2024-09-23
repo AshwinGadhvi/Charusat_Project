e76dc688f898705be0180869c4cbc537af940a01
AshwinGadhvi <ashwingadhvi397@gmail.com>
2024-09-23 09:30:31 +0530

    Backend Code For Otp Verification.

--- Admin/login-form-20/otp.aspx.vb
+++ Admin/login-form-20/otp.aspx.vb
@@ -1,18 +1,12 @@
 ﻿
 Partial Class login_form_20_otp
     Inherits System.Web.UI.Page
-
-    Private Sub otp_Load(sender As Object, e As EventArgs) Handles otp.Load
-        If Not Page.IsPostBack Then
-            ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "swal('OTP Sent!', 'The OTP Has been Sent to the Specified E-mail!', 'success');", True)
-        End If
-    End Sub
-
-    Private Sub Confirm_Click(sender As Object, e As EventArgs) Handles Confirm.Click
-        If otp.Text = Session("Otp") Then
+    Private Sub submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+        If otp.Text = Session("otp") Then
             Response.Redirect("Change_Password.aspx")
         Else
             ScriptManager.RegisterStartupScript(Me, Me.GetType(), "alertMessage", "swal('Wrong OTP!', 'The OTP is Not Correct!', 'error');", True)
         End If
     End Sub
+
 End Class
